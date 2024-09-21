f5e18a4c3ddac18849c1f6a2c3ca7434052ef9c5
Ihor <enotshow275@gmail.com>
2024-09-21 11:49:27 +0200

    Keyring credentials

--- aws_client.py
+++ aws_client.py
@@ -1,22 +1,31 @@
 import os
 
 import boto3
+import keyring
 
 from exceptions import CredentialsError
 
 
 class AWSClient:
+    def __init__(self, service_name="aws_s3"):
+        self.service_name = service_name
 
     def auth(self, aws_access_key_id, aws_secret_access_key):
-        os.environ['AWS_ACCESS_KEY_ID'] = aws_access_key_id
-        os.environ['AWS_SECRET_ACCESS_KEY'] = aws_secret_access_key
+        """
+        Store AWS credentials securely using the keyring.
+        """
+        keyring.set_password(self.service_name, "AWS_ACCESS_KEY_ID", aws_access_key_id)
+        keyring.set_password(self.service_name, "AWS_SECRET_ACCESS_KEY", aws_secret_access_key)
 
     def get_client(self):
-        access_key = os.getenv('AWS_ACCESS_KEY_ID')
-        secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+        """
+        Retrieve AWS credentials from keyring and create a boto3 client.
+        """
+        access_key = keyring.get_password(self.service_name, "AWS_ACCESS_KEY_ID")
+        secret_key = keyring.get_password(self.service_name, "AWS_SECRET_ACCESS_KEY")
 
         if not access_key or not secret_key:
-            raise CredentialsError("Credentials not provided")
+            raise CredentialsError("Credentials not found in keyring. Please authenticate first.")
 
         return boto3.client(
             's3',
