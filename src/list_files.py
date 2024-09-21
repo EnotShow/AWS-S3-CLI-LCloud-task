2d47b0277e70661c12b2342c3cda55eb0d0fb401
Ihor <enotshow275@gmail.com>
2024-09-21 11:18:37 +0200

    list filter
    - Added "regex" option to list method

--- src/list_files.py
+++ src/list_files.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 import click
 from botocore.exceptions import ClientError
@@ -9,13 +10,19 @@ from exceptions import CredentialsError
 
 @click.command(name='list')
 @click.option('--bucket', required=True, help='The name of the S3 bucket.')
-def list_files(bucket):
+@click.option('--regex', required=False, help='Specify a file regex filter.')
+def list_files(bucket, regex):
     """List all files in an S3 bucket."""
     try:
         s3 = aws_client.get_client()
 
         response = s3.list_objects_v2(Bucket=bucket)
         if 'Contents' in response:
+            if regex:
+                pattern = re.compile(regex)
+                for obj in response['Contents']:
+                    if pattern.match(obj['Key']):
+                        print(obj['Key'])
             for obj in response['Contents']:
                 print(obj['Key'])
         else:
@@ -23,7 +30,6 @@ def list_files(bucket):
     except ClientError as e:
         print(f"Error listing files: {e}")
         sys.exit(1)
-
     except CredentialsError as e:
         print(e)
         sys.exit(1)
